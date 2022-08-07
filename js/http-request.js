4c8aac5b2d0267241416efa2b3ee85db4d1fa347
Daria <daria.bnn@bk.ru>
2022-08-07 22:23:23 +0300

    replacement XMLHttpRequest -> fetch

--- js/http-request.js
+++ js/http-request.js
@@ -1,38 +1,6 @@
-const noop = () => { };
-
-function httpRequest({
-    method = 'GET',
-    headers,
-    url,
-    body,
-    type = 'json',
-    onSuccess = noop,
-    onError = function () {
-        window.application.renderScreen('screen-error-request')
-    },
-}) {
-    const request = new XMLHttpRequest();
-
-    request.open(method, url);
-    request.responseType = type;
-
-    if (headers) {
-        Object.keys(headers).forEach(headerName => {
-            request.setRequestHeader(headerName, headers[headerName]);
-        });
-    }
-
-    request.send(body);
-
-    request.onload = function () {
-        if (request.status === 200) {
-            onSuccess(request.response);
-        } else {
-            onError('Не удалось отправить запрос');
-        }
-    };
-
-    request.onerror = function () {
-        onError('Отсутсвует интернет');
-    }
+function fetchRequest(url, cb) {
+  return fetch(url)
+    .then((result) => result.json())
+    .then((data) => cb(data))
+    .catch(() => window.application.renderScreen("screen-error-request"));
 }
